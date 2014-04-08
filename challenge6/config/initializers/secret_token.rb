1345b9549603563dd57225b07d9bb58eded5c080
Rob Ring <rjm.ring@gmail.com>
2014-04-08 12:35:22 -0500

    First commit

---
+++ challenge6/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge6::Application.config.secret_key_base = 'aa42652fb082f88954ea35d8811e867504a8093dd499ba9d4f3c71733c2c3060976e8678846dd86cb900b49198ba83327e6fa2e42cfb20f2e9dd2c0f9b554eb1'
